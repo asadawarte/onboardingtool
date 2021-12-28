67b8759e0f1400e05615a3c31dea96ebd38ed6f8
asadawarte <95573012+asadawarte@users.noreply.github.com>
2021-12-28 18:11:58 +0530

    icons added

--- components/header.js
+++ components/header.js
@@ -1,15 +1,36 @@
 
 import { CgProfile } from "react-icons/cg";
 import React from 'react'
+import { Dropdown } from "react-bootstrap";
 
 class Header extends React.Component {
+
+
+    openDropdown(){
+        return ;
+    }
 
     render(){
         return (
             <div>
                <div className="header">
+
                     <div style={{float:'right',paddingRight:"10px"}}>
-                        <CgProfile  style={{fontSize:'30px'}}></CgProfile>
+                        <CgProfile onClick={this.openDropdown} style={{fontSize:'30px'}}>
+                          <Dropdown>
+                            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                Dropdown Button
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                            </Dropdown.Menu>
+                      </Dropdown>
+
+                            
+                        </CgProfile>
                     </div>
               </div>
             </div>
